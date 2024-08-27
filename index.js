734ae10fbe0651cdf2426bba4770d49d7d491775
yynnw <165120529+yynnw@users.noreply.github.com>
2024-08-26 23:59:52 -0400

    Update index.js
    change port from hardcoded to dynamic for render.com deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const posts = []
